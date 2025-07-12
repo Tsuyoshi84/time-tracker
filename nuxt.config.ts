0f9853861290ebb83be7f298fc8fbbad1735f7e1
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2025-07-12 20:42:19 +0900

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
+  future: {
+    compatibilityVersion: 4,
+  },
+  srcDir: "app/",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ["@nuxt/eslint"],
+  ssr: false,
 })
