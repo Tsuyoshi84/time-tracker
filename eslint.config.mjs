2f16f8a6abbe54f2abdb8db8bece43efbadb8e0b
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2026-06-30 07:42:52 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,13 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
 	rules: {
 		'no-self-assign': 'off', // Checked by Biome
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'any',
+				},
+			},
+		],
 	},
 })
